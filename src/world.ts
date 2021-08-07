34146a07e949d69e6a390cb01ba5c04f5f5691f0
ayumitanaka <tanakayumi13@gmail.com>
2021-08-07 22:43:20 +0000

    test

--- src/world.ts
+++ src/world.ts
@@ -5,7 +5,7 @@ export default class World {
     this.message = message
   }
 
-  public sayHello(element: HTMLElement | null): void {
+  public sayHello(element: HTMLElement | null) {
     if (element) {
       element.innerText = this.message
     }
